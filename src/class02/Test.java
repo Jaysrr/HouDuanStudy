96e7df3635e4d783b01d5453a206ea7180d4e289
Jaysrr <974054347@qq.com>
2020-06-17 20:33:55 +0800

    更新Book作者信息

--- src/class02/Test.java
+++ src/class02/Test.java
@@ -13,20 +13,26 @@ import java.util.List;
 public class Test {
     public static void main(String[] args) {
         Book book = new Book();
-        book.setAuthor(new ArrayList<>());
+
+        List<String> authors = new ArrayList();
+        authors.add("Robort");
+        authors.add("周志明");
+        book.setAuthor(authors);
+
         book.setDepth(105.2d);
         book.setPrintingTime(new Date());
         book.setPublicationTime(new Date());
-        book.setVersion(1);
+        book.setVersion(1.0);
 
         Book book2 = new Book();
         book2.setAuthor(new ArrayList<>());
         book2.setDepth(88.5d);
         book2.setPrintingTime(new Date());
         book2.setPublicationTime(new Date());
-        book2.setVersion(8);
+        book2.setVersion(8.0);
 
         System.out.println(book);
+        System.out.println(book2);
         List<Book> books = new ArrayList<>();
         books.add(book);
         System.out.println(books);
